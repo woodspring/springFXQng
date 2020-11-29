446e717a738e69ee60427cd2ab0361195e5f6a1e
Alfred Huang <woodspring.toronto@gmail.com>
2020-11-28 23:44:43 -0500

    M app_module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +22,9 @@ import { FxqDetailComponent } from './components/fxq-detail/fxq-detail.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/springfxq.service.ts
+++ src/app/services/springfxq.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpringfxqService {
+  private  fxqUrl = 'http://192.168.0.161:4983/FXQ/';
+  constructor(private http: HttpClient) {  }
 
-  constructor() { }
+  getAll(): Observable<any> {
+    return this.http.get( '${fxqUrl}/slist');
+  }
+
+  getById(id: number): Observable<any> {
+    return this.http.get( '${fxqUrl}/id/${id}');
+  }
+
+  findBySymbol(symbol: string): Observable<any> {
+    return this.http.get( '${fxqUrl}/symbols/${symbol}');
+  }
+
+  findBySymbolTenor(symbol: string, tenor: string): Observable<any> {
+    return this.http.get( '${fxqUrl}/symbols/${symbol}/tenor/${tenor}');
+  }
+
 }
